30e0bacb62f8d2164ead1c35ca64676df0ed4e7d
John Doe <johndoe@johndoe@student.nhh.no>
2024-10-01 14:56:01 +0200

    All answer uploads, see the seperate txt file for answers and a summary of the output

--- part_A.py
+++ part_A.py
@@ -1,39 +1,4 @@
-"""
-TECH2 mandatory assignment - Part A
-
-Write the implementation of part A of the exercise below.
-"""
-
-def std_loops(x):
-    """
-    Compute standard deviation of x using loops.
-
-    Parameters
-    ----------
-    x: Sequence of numbers
-
-    Returns
-    -------
-    sd : float
-        Standard deviation of the list of numbers.
-    """
-
-def std_builtin(x):
-    """
-    Compute standard deviation of x using the built-in functions sum()
-    and len().
-
-    Parameters
-    ----------
-    x: Sequence of numbers
-
-    Returns
-    -------
-    sd : float
-        Standard deviation of the list of numbers.
-    """
-# Either I'm too stupid to understand or haven't gotten proper education to use the def/functions above.
-# Nevertheless, here are my answers and step-wise explanations for each of the 3 methods:
+# TECH2 mandatory assignment - Part A
 
 # ****** Method 1: using loops  and only the function len() ******
 
